38685d1bb9625857fb729d9897f066f09e43fe23
fajarrijki27 <fajarrijkipaujan2007@gmail.com>
2024-08-23 15:10:30 +0700

    Latihan Laragon

---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydatabase";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+echo "Koneksi berhasil<br>";
+
+// Mengambil dan menampilkan data dari tabel users
+$sql = "SELECT id, name, email FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Menutup koneksi
+$conn->close();
+?>
